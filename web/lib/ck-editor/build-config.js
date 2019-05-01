97c4ef573df2affffc5ea05a149593620b336dfc
viipe94 <viiper94@gmail.com>
2019-05-01 16:27:37 +0300

    init repo

---
+++ web/lib/ck-editor/build-config.js
@@ -0,0 +1,81 @@
+﻿/**
+ * @license Copyright (c) 2003-2017, CKSource - Frederico Knabben. All rights reserved.
+ * For licensing, see LICENSE.md or http://ckeditor.com/license
+ */
+
+/**
+ * This file was added automatically by CKEditor builder.
+ * You may re-use it at any time to build CKEditor again.
+ *
+ * If you would like to build CKEditor online again
+ * (for example to upgrade), visit one the following links:
+ *
+ * (1) http://ckeditor.com/builder
+ *     Visit online builder to build CKEditor from scratch.
+ *
+ * (2) http://ckeditor.com/builder/3ff7615263cd3ded5ba070606af1c3b7
+ *     Visit online builder to build CKEditor, starting with the same setup as before.
+ *
+ * (3) http://ckeditor.com/builder/download/3ff7615263cd3ded5ba070606af1c3b7
+ *     Straight download link to the latest version of CKEditor (Optimized) with the same setup as before.
+ *
+ * NOTE:
+ *    This file is not used by CKEditor, you may remove it.
+ *    Changing this file will not change your CKEditor configuration.
+ */
+
+var CKBUILDER_CONFIG = {
+	skin: 'minimalist',
+	preset: 'basic',
+	ignore: [
+		'.DS_Store',
+		'.bender',
+		'.editorconfig',
+		'.gitattributes',
+		'.gitignore',
+		'.idea',
+		'.jscsrc',
+		'.jshintignore',
+		'.jshintrc',
+		'.mailmap',
+		'bender-err.log',
+		'bender-out.log',
+		'bender.js',
+		'dev',
+		'gruntfile.js',
+		'less',
+		'node_modules',
+		'package.json',
+		'tests'
+	],
+	plugins : {
+		'autolink' : 1,
+		'basicstyles' : 1,
+		'clipboard' : 1,
+		'enterkey' : 1,
+		'entities' : 1,
+		'find' : 1,
+		'floatingspace' : 1,
+		'htmlwriter' : 1,
+		'indentlist' : 1,
+		'link' : 1,
+		'list' : 1,
+		'maximize' : 1,
+		'pastefromexcel' : 1,
+		'pastefromword' : 1,
+		'pastetext' : 1,
+		'removeformat' : 1,
+		'resize' : 1,
+		'sourcearea' : 1,
+		'specialchar' : 1,
+		'spoiler' : 1,
+		'table' : 1,
+		'toolbar' : 1,
+		'undo' : 1,
+		'wysiwygarea' : 1
+	},
+	languages : {
+		'en' : 1,
+		'ru' : 1
+	}
+};
---
+++ web/lib/ck-editor/skins/minimalist/readme.md
@@ -0,0 +1,8 @@
+
+"Minimalist" Skin
+====================
+
+A port of the default CKEditor skin, Moono, without the gradients.
+
+Jeff Lyon
+[Albatross Digital](albatrossdigital.com)
